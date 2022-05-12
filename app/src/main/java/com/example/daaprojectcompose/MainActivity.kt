9534738355e87a50e52dea3ad21d9f2253b12b11
Srinidhi.R <20010114475.SRINIDHI@cse.reva.edu.in>
2022-05-12 15:16:40 +0530

    first android

---
+++ app/src/main/java/com/example/daaprojectcompose/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.daaprojectcompose
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.daaprojectcompose.ui.theme.DAAprojectcomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DAAprojectcomposeTheme {
+                val inputValue = remember {
+                    mutableStateOf("")
+                }
+                val viewModel : ViewModel = ViewModel()
+                val sortedArray by viewModel._array.observeAsState()
+                val listValue = remember {
+                    mutableListOf<String>()
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(
+                                LocalConfiguration.current.screenWidthDp.dp / 8
+                            )
+                            .width(LocalConfiguration.current.screenHeightDp.dp / 8)
+                    )
+                    Spacer(modifier = Modifier.padding(LocalConfiguration.current.screenHeightDp.dp/45))
+                    Text(text = "Enter your numbers that needs to be sorted")
+                    Spacer(modifier = Modifier.padding(LocalConfiguration.current.screenHeightDp.dp/45))
+                    OutlinedTextField(value = inputValue.value, onValueChange = {inputValue.value = it}, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+                    Spacer(modifier = Modifier.padding(LocalConfiguration.current.screenHeightDp.dp/45))
+                    Row(
+                        modifier = Modifier
+                            .width(LocalConfiguration.current.screenWidthDp.dp - 40.dp)
+                            .height(
+                                LocalConfiguration.current.screenHeightDp.dp / 30
+                            )
+                    ) {
+                        sortedArray?.forEach { it->
+                            element(element = it)
+                        }
+                    }
+                    Button(onClick = {
+                        val li = inputValue.value.split(" ".toRegex())
+                        li.forEachIndexed{ i, s ->
+                            listValue.add(i,s)
+                        }
+                        viewModel.shellSort(listValue)
+                        Toast.makeText(this@MainActivity,"${sortedArray?.toString()}", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text(text = "SHELL SORT")
+                    }
+                }
+            }
+        }
+    }
+}
+@Composable
+fun element(
+    element:String
+) {
+    Box(modifier = Modifier
+        .size(30.dp)
+        .clip(RoundedCornerShape(10.dp))
+        .background(Color.Blue), contentAlignment = Alignment.Center){
+        Text(text = "$element", fontSize = 15.sp, style = TextStyle(color = Color.White))
+    }
+}
---
+++ app/src/main/java/com/example/daaprojectcompose/ViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.daaprojectcompose
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ViewModel: ViewModel() {
+
+    private val array1 = MutableLiveData<List<String>>()
+    val _array: LiveData<List<String>> = array1
+
+
+    fun shellSort(list: List<String>) {
+        var array = list.toTypedArray()
+        var h = 1
+        while (h < list.size / 3) {
+            h = h * 3 + 1
+        }
+
+        while (h >= 1) {
+            for (i in h until list.size) {
+                for (j in i downTo h step h) {
+                    if (array[j - h] < array[j]) break
+                    var temp = array[j]
+                    array[j] = array[j-h]
+                    array[j-h]= temp.toString()
+                }
+            }
+            h /= 3
+        }
+        array1.value = array.toList()
+    }
+
+
+}
